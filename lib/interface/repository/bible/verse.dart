74a3ad81201bedf1a204399d75808db6d4ae7eaf
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-04-09 13:26:33 -0300

    repository add param to searchVerse

--- lib/interface/repository/bible/verse.dart
+++ lib/interface/repository/bible/verse.dart
@@ -37,15 +37,27 @@ abstract class InterfaceRepositoryVerse {
 
   Future<int> isDatabaseEmpty();
 
-  /// This method returns a list of verses that match the given search tokens
-  /// and the maximum number of results.
+  /// Searches for verses that match the given tokens.
   ///
-  /// The search is performed in the specified division, book and bibleVersion if provided.
+  /// * The `tokens` parameter is a list of strings that represent the tokens to search for in the verse content.
   ///
-  /// If no division or book or bibleVersion is provided, the search is performed in all divisions and all books and all bibleVersions.
+  /// * The `maxResults` parameter is an optional integer that specifies the maximum number of results to return.
+  ///
+  /// * The `pagination` parameter is an optional integer that specifies the pagination value.
+  ///
+  /// * The `division` parameter is an optional [Division] object that represents the division of the book.
+  ///
+  /// * The `book` parameter is an optional [Book] object that represents the book of the verse.
+  ///
+  /// * The `bibleVersion` parameter is an optional [BibleVersion] object that represents the bible version.
+  ///
+  /// * The `containAllTokens` parameter is an optional boolean that specifies whether all tokens should be found in the verse content.
+  ///
+  ///   * if now passed, the query will search using **OR** operator between all tokens.
+  ///
+  /// Returns a `Future` that resolves to a `List` of [VerseView] objects that match the search criteria.
   ///
   /// The search is case-insensitive.
-  ///
   Future<List<VerseView>> searchVerse({
     required List<String> tokens,
     int? maxResults,
@@ -53,5 +65,6 @@ abstract class InterfaceRepositoryVerse {
     Division? division,
     Book? book,
     BibleVersion? bibleVersion,
+    bool containAllTokens = false,
   });
 }
