327fdf3938ec3655fc5640fc8e3c5222d8d70d31
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-05-27 11:17:42 -0300

    Separeted the model from the interface core

--- ios/Flutter/flutter_export_environment.sh
+++ ios/Flutter/flutter_export_environment.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 # This is a generated file; do not edit or check into version control.
 export "FLUTTER_ROOT=C:\Desenvolvimento\framework\flutter"
-export "FLUTTER_APPLICATION_PATH=D:\Desenvolvimento\Source - Flutter\aletheia-core-model"
+export "FLUTTER_APPLICATION_PATH=D:\Desenvolvimento\Source - Aletheia\app\core\aletheia-core-model"
 export "COCOAPODS_PARALLEL_CODE_SIGN=true"
 export "FLUTTER_TARGET=lib\main.dart"
 export "FLUTTER_BUILD_DIR=build"
-export "FLUTTER_BUILD_NAME=0.0.22"
-export "FLUTTER_BUILD_NUMBER=0.0.22"
+export "FLUTTER_BUILD_NAME=0.0.27"
+export "FLUTTER_BUILD_NUMBER=0.0.27"
 export "DART_OBFUSCATION=false"
 export "TRACK_WIDGET_CREATION=true"
 export "TREE_SHAKE_ICONS=false"
