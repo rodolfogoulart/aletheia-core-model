97dee3176ac462f73c1f89322c94c7bc5879ebd7
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-06-23 21:59:55 -0300

    TypeModules

---
+++ lib/src/model/modules/modules.dart
@@ -0,0 +1,26 @@
+enum TypeModules {
+  bible,
+  commentary,
+  dictionary,
+  lexico,
+  nlp,
+  user,
+  ;
+
+  toMap() {
+    return name;
+  }
+
+  factory TypeModules.fromMap(value) {
+    return TypeModules.values.byName(value);
+  }
+}
+
+extension TypeModulesExtension on TypeModules {
+  bool get isBible => this == TypeModules.bible;
+  bool get isCommentary => this == TypeModules.commentary;
+  bool get isDictionary => this == TypeModules.dictionary;
+  bool get isLexico => this == TypeModules.lexico;
+  bool get isNlp => this == TypeModules.nlp;
+  bool get isUser => this == TypeModules.user;
+}
