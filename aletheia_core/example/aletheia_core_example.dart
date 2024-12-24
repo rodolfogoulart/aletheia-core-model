56ba22b0f3ee37c97b9caea71702b0ac2fb0b3e7
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-12-24 17:20:55 -0300

    move package to folder

---
+++ aletheia_core/example/aletheia_core_example.dart
@@ -0,0 +1,3 @@
+void main() {
+  print('Welcome to Aletheia Core');
+}
--- lib/aletheia_core.dart
+++
@@ -1,6 +0,0 @@
-library aletheia_core;
-
-export 'aletheia_core.dart';
-export 'aletheia_core_model.dart';
-export 'aletheia_core_interface_controller.dart';
-export 'aletheia_core_interface_repository.dart';
