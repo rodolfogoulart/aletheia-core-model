f4aa7b80415910075fb9357c8adfb5bc551e7a2f
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2024-12-24 17:06:32 -0300

    .

---
+++ lib/aletheia_core.dart
@@ -0,0 +1,6 @@
+library aletheia_core;
+
+export 'aletheia_core.dart';
+export 'aletheia_core_model.dart';
+export 'aletheia_core_interface_controller.dart';
+export 'aletheia_core_interface_repository.dart';
