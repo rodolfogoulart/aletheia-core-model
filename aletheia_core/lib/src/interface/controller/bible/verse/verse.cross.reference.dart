b08de714841cfbb5c0ea81941d560e180e81c12e
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2025-03-07 18:18:44 -0300

    flag useIsolate

--- aletheia_core/lib/src/interface/controller/bible/verse/verse.cross.reference.dart
+++ aletheia_core/lib/src/interface/controller/bible/verse/verse.cross.reference.dart
@@ -6,6 +6,7 @@ abstract class InterfaceControllerCrossReference {
     required BibleVersion bibleVersion,
     // required int chapter,
     required VerseView verse,
+    bool useIsolate = true,
   });
 
   Future<int> setVerseCrossReference({
