2e933ed2a71d4f9714eacb2e7660002845008cd7
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2025-04-30 21:41:41 -0300

    fix conversion BookView

--- aletheia_core/lib/src/interface/controller/bible/verse/verse.dart
+++ aletheia_core/lib/src/interface/controller/bible/verse/verse.dart
@@ -42,8 +42,12 @@ abstract class InterfaceControllerVerse {
   Future<List<VerseView>> getVerse(
       {required int idBibleVersion, required int idBook, required int numberChapter, required int numberVerse});
 
+  /// Get the number of verses in a chapter of a specific version
+  ///
+  /// if version is not relevant, try to use [getNumberVersesOnChapter]
   Future<int> getVerseCountFromBookChapter({required int idBook, required int numberChapter, required int idBibleVersion});
 
+  /// set the cross reference for a verse
   Future<int> setVerseCrossReference({
     required int idBook,
     required int numberVerse,
@@ -55,40 +59,17 @@ abstract class InterfaceControllerVerse {
     required int? numberVerseReferenceEnd,
   });
 
-  //MARK: searchVerse
-  ///Searches for verses that match the given tokens.
+  /// Get verses that contain the lexico
   ///
-  ///The `tokens` parameter is a list of strings that represent the tokens to search for in the verse content.
+  /// `idLexico` is the id of the lexico start with G or H (G for greek and H for hebrew)
   ///
-  ///The `maxResults` parameter is an optional integer that specifies the maximum number of results to return.
+  /// `idBibleVersion` is the id of the bible version
   ///
-  ///The `pagination` parameter is an optional integer that specifies the pagination value.
+  /// `return` a list of VerseView that contain the lexico
   ///
-  ///The `division` parameter is an optional [Division] object that represents the division of the book.
-  ///
-  ///The `book` parameter is an optional [Book] object that represents the book of the verse.
-  ///
-  ///The `bibleVersion` parameter is an optional [BibleVersion] object that represents the bible version.
-  ///
-  ///The `containAllTokens` parameter is an optional boolean that specifies whether all tokens should be found in the verse content.
-  ///
-  ///if now passed, the query will search using OR operator between all tokens.
-  ///Returns a Future that resolves to a List of [VerseView] objects that match the search criteria.
-  ///
-  ///The search is case-insensitive.
-  // Future<List<SearchVerse>> searchVerse({
-  //   required List<String> tokens,
-  //   int? maxResults = 50,
-  //   int pagination = 0,
-  //   Division? division,
-  //   Book? book,
-  //   BibleVersion? bibleVersion,
-  //   required BibleVersion defaultbibleVersion,
-  //   // bool containAllTokens = false,
-  //   required StreamController onCompute,
-  // });
-
   Future<List<VerseView>> getVersesByLexico({required String idLexico, required BibleVersion idBibleVersion});
 
+  /// Get the number of verses in a chapter
+  ///
   Future<int> getNumberVersesOnChapter({required idBook, required chapter});
 }
