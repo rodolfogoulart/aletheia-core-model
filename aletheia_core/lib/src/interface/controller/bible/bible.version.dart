b96f68689522f27b4cf751bacb9129646023681f
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2025-05-02 19:37:21 -0300

    fix InterfaceControllerBibleVersion

--- aletheia_core/lib/src/interface/controller/bible/bible.version.dart
+++ aletheia_core/lib/src/interface/controller/bible/bible.version.dart
@@ -1,8 +1,8 @@
 import 'package:aletheia_core/aletheia_core_model.dart';
 
 abstract class InterfaceControllerBibleVersion {
-  Future<BibleVersion> getBibleVersionById(id);
-  Future<BibleVersion> getBibleVersionByHash(String hash);
+  Future<BibleVersion?> getBibleVersionById(id);
+  Future<BibleVersion?> getBibleVersionByHash(String hash);
   Future<List<BibleVersion>> get getAllBibleVersions;
   Future<List<BibleVersionBooksView>> getAllBibleVersionBooksView();
   String getHashBibleVersion({required String abrev, required String language});
