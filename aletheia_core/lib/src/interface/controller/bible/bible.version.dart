db19e4fe9209198b9db33c0e003eb53be0d8a730
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2025-05-02 19:18:23 -0300

    add getBibleVersionById, getBibleVersionByHash

--- aletheia_core/lib/src/interface/controller/bible/bible.version.dart
+++ aletheia_core/lib/src/interface/controller/bible/bible.version.dart
@@ -1,10 +1,12 @@
 import 'package:aletheia_core/aletheia_core_model.dart';
 
 abstract class InterfaceControllerBibleVersion {
+  Future<BibleVersion> getBibleVersionById(id);
+  Future<BibleVersion> getBibleVersionByHash(String hash);
   Future<List<BibleVersion>> get getAllBibleVersions;
   Future<List<BibleVersionBooksView>> getAllBibleVersionBooksView();
-
   String getHashBibleVersion({required String abrev, required String language});
+
   Future<int> setBibleVersion(BibleVersion bibleVersion);
   Future<int> setBibleVersionBook(BibleVersionBooks bibleVersionBooks);
 
