e6ef067f6b0d3a3a81bf07b7c604d6f9f1518abc
Rodolfo Goulart <rodolfo.goulart@gmail.com>
2026-01-16 15:13:27 -0300

    Add JSON serialization to VerseAudio class
    Introduced toMap, fromMap, toJson, and fromJson methods to the VerseAudio class to support JSON serialization and deserialization of audio data.

--- aletheia_core/lib/src/model/bible/verse/verse.audio.dart
+++ aletheia_core/lib/src/model/bible/verse/verse.audio.dart
@@ -1,3 +1,5 @@
+import 'dart:convert';
+
 /// Represents audio data for a Bible verse
 class VerseAudio {
   List<int> audioBytes;
@@ -6,4 +8,22 @@ class VerseAudio {
     required this.audioBytes,
     required this.audioFormat,
   });
+
+  factory VerseAudio.fromMap(Map<String, dynamic> json) => VerseAudio(
+        audioBytes: List<int>.from(json['audioBytes']),
+        audioFormat: json['audioFormat'],
+      );
+
+  Map<String, dynamic> toMap() {
+    final result = <String, dynamic>{};
+
+    result.addAll({'audioBytes': audioBytes});
+    result.addAll({'audioFormat': audioFormat});
+    return result;
+  }
+
+  String toJson() => json.encode(toMap());
+
+  factory VerseAudio.fromJson(String source) =>
+      VerseAudio.fromMap(json.decode(source) as Map<String, dynamic>);
 }
